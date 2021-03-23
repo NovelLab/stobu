cc3f108db3dbab694ccd7b2355591ba46d18d220
nagisc007 <nagisc007@yahoo.co.jp>
2021-03-23 10:12:34 +0900

    Added: scene info

--- stobu/commands/projectbuilder.py
+++ stobu/commands/projectbuilder.py
@@ -11,21 +11,44 @@ from stobu.core.basedatabuilder import on_build_basedata
 from stobu.core.novelbuilder import on_build_novel
 from stobu.core.outlinebuilder import on_build_outline
 from stobu.core.plotbuilder import on_build_plot
+from stobu.core.sceneinfobuilder import on_build_scene_info
 from stobu.core.scriptbuilder import on_build_script
 from stobu.core.storydatacreator import get_story_data
 from stobu.datatypes import StoryData
 from stobu.datatypes import OutputData
 from stobu.nametagmanager import get_nametag_db
+from stobu.settings import MARKDOWN_EXT
 from stobu.tools import pathmanager as ppath
 from stobu.util import assertion
 from stobu.util.dicts import dict_sorted
 from stobu.util.fileio import write_file
+from stobu.util.filepath import add_extention
 from stobu.util.log import logger
 
 
 __all__ = (
         'switch_command_to_build',
         )
+
+
+# Define constants
+FILENAME_BASEDATA = add_extention('data', MARKDOWN_EXT)
+"""str: file name of base data."""
+
+FILENAME_OUTLINE = add_extention('outline', MARKDOWN_EXT)
+"""str: file name of outline."""
+
+FILENAME_PLOT = add_extention('plot', MARKDOWN_EXT)
+"""str: file name of plot."""
+
+FILENAME_SCRIPT = add_extention('script', MARKDOWN_EXT)
+"""str: file name of script."""
+
+FILENAME_NOVEL = add_extention('novel', MARKDOWN_EXT)
+"""str: file name of novel."""
+
+FILENAME_SCENE_INFO = add_extention('scene', MARKDOWN_EXT)
+"""str: file name of scene info."""
 
 
 # Main Functions
@@ -45,7 +68,7 @@ def switch_command_to_build(cmdargs: argparse.Namespace) -> bool:
         outline_data = assertion.is_instance(on_build_outline(story_data, tagdb),
                 OutputData)
         if outline_data:
-            path = os.path.join(ppath.get_build_dir_path(), "outline.md")
+            path = os.path.join(ppath.get_build_dir_path(), FILENAME_OUTLINE)
             if not write_file(path, outline_data.get_serialized_data()):
                 logger.error("...Failed to write the output outline data!")
                 return False
@@ -61,7 +84,7 @@ def switch_command_to_build(cmdargs: argparse.Namespace) -> bool:
         plot_data = assertion.is_instance(on_build_plot(story_data, tagdb),
                 OutputData)
         if plot_data:
-            path = os.path.join(ppath.get_build_dir_path(), "plot.md")
+            path = os.path.join(ppath.get_build_dir_path(), FILENAME_PLOT)
             if not write_file(path, plot_data.get_serialized_data()):
                 logger.error("...Failed to write the output plot data!")
                 return False
@@ -71,6 +94,23 @@ def switch_command_to_build(cmdargs: argparse.Namespace) -> bool:
             logger.error("...Failed to build the output data in plot process!: %s", plot_data)
             return False
 
+    # - scene info
+    if cmdargs.info:
+        logger.debug("Building scene info...")
+        data = assertion.is_instance(
+                on_build_scene_info(story_data, tagdb),
+                OutputData)
+        if data:
+            path = os.path.join(ppath.get_build_dir_path(), FILENAME_SCENE_INFO)
+            if not write_file(path, data.get_serialized_data()):
+                logger.error("...Failed to write the output scene info data!")
+                return False
+            else:
+                logger.debug("...Succeeded to output the scene info data.")
+        else:
+            logger.error("...Failed to build the output data in scene info process!: %s", data)
+            return False
+
     # - script
     if cmdargs.script:
         logger.debug("Building script data...")
@@ -78,7 +118,7 @@ def switch_command_to_build(cmdargs: argparse.Namespace) -> bool:
                 on_build_script(story_data, tagdb, cmdargs.rubi),
                 OutputData)
         if script_data:
-            path = os.path.join(ppath.get_build_dir_path(), "script.md")
+            path = os.path.join(ppath.get_build_dir_path(), FILENAME_SCRIPT)
             if not write_file(path, script_data.get_serialized_data()):
                 logger.error("...Failed to write the output script data!")
                 return False
@@ -95,7 +135,7 @@ def switch_command_to_build(cmdargs: argparse.Namespace) -> bool:
                 on_build_novel(story_data, tagdb, cmdargs.rubi),
                 OutputData)
         if novel_data:
-            path = os.path.join(ppath.get_build_dir_path(), "novel.md")
+            path = os.path.join(ppath.get_build_dir_path(), FILENAME_NOVEL)
             if not write_file(path, novel_data.get_serialized_data()):
                 logger.error("...Failed to write the output novel data!")
                 return False
@@ -109,7 +149,7 @@ def switch_command_to_build(cmdargs: argparse.Namespace) -> bool:
     base_data = assertion.is_instance(on_build_basedata(cmdargs, story_data, tagdb),
             OutputData)
     if base_data:
-        path = os.path.join(ppath.get_build_dir_path(), "data.md")
+        path = os.path.join(ppath.get_build_dir_path(), FILENAME_BASEDATA)
         if not write_file(path, base_data.get_serialized_data()):
             logger.error("...Failed to write the output base data!")
             return False
---
+++ stobu/core/sceneinfobuilder.py
@@ -0,0 +1,127 @@
+"""Building module for scene info."""
+
+
+# Official Libraries
+import copy
+
+
+# My Modules
+from stobu.core.actiondatacreator import get_action_data
+from stobu.core.contentscreator import get_contents_list
+from stobu.core.rubibuilder import apply_rubi_convert
+from stobu.core.storycodecreator import get_story_code_data
+from stobu.dataconverter import conv_text_in_action_data_by_tags, conv_text_list_by_tags
+from stobu.datatypes import ActionData, ActionRecord
+from stobu.datatypes import OutputData
+from stobu.datatypes import StoryCodeData, StoryCode
+from stobu.datatypes import StoryData
+from stobu.formatter import format_contents_table_data, format_scene_info_data, get_breakline
+from stobu.instructions import apply_instruction_to_action_data
+from stobu.nametagmanager import NameTagDB
+from stobu.util import assertion
+from stobu.util.log import logger
+
+
+__all__ = (
+        'on_build_scene_info',
+        )
+
+
+# Main Functions
+def on_build_scene_info(story_data: StoryData, tags: dict) -> OutputData:
+    assert isinstance(story_data, StoryData)
+    assert isinstance(tags, dict)
+
+    contents = get_contents_list(story_data)
+
+    action_data = assertion.is_instance(get_action_data_tagfixed(story_data, tags),
+            ActionData)
+
+    story_code_data = assertion.is_instance(
+            get_story_code_from_action_data(action_data), StoryCodeData)
+
+    info_data = format_contents_table_data(contents) \
+            + ['\n', get_breakline()] \
+            + format_scene_info_data(story_code_data, tags)
+
+    output_data = conv_text_list_by_tags(info_data, tags)
+
+    return OutputData(output_data)
+
+
+# Functions
+def get_action_data_tagfixed(story_data: StoryData, tags: dict) -> ActionData:
+    assert isinstance(story_data, StoryData)
+    assert isinstance(tags, dict)
+
+    action_data = assertion.is_instance(get_action_data(story_data), ActionData)
+
+    action_data_applied = assertion.is_instance(
+            apply_instruction_to_action_data(action_data), ActionData)
+
+    callings = NameTagDB.get_calling_tags()
+    action_data_tagfixed = assertion.is_instance(
+            conv_text_in_action_data_by_tags(action_data, callings),
+            ActionData)
+
+    return action_data_tagfixed
+
+
+def get_story_code_from_action_data(action_data: ActionData) -> StoryCodeData:
+    assert isinstance(action_data, ActionData)
+
+    tmp = []
+    scene_data = {
+            'camera': '',
+            'stage': '',
+            'year': '',
+            'date': '',
+            'time': '',
+            }
+    elapsed = {
+            'camera': '',
+            'stage': '',
+            'year': '',
+            'date': '',
+            'time': '',
+            }
+    for record in action_data.get_data():
+        assert isinstance(record, ActionRecord)
+
+        if 'title' in record.type:
+            tmp.append(StoryCode(record.type, record.outline, record.note))
+        elif record.type in ('scene-camera', 'scene-stage', 'scene-year', 'scene-date', 'scene-time'):
+            tmp.append(StoryCode(record.type, record.subject, record.note))
+            if record.type == 'scene-camera':
+                elapsed['camera'] = 'changed' if scene_data['camera'] != record.subject else ''
+                scene_data['camera'] = record.subject
+            elif record.type == 'scene-stage':
+                elapsed['stage'] = 'changed' if scene_data['stage'] != record.subject else ''
+                scene_data['stage'] = record.subject
+            elif record.type == 'year':
+                elapsed['year'] = 'changed' if scene_data['year'] != record.subject else ''
+                scene_data['year'] = record.subject
+            elif record.type == 'date':
+                elapsed['date'] = 'changed' if scene_data['date'] != record.subject else ''
+                scene_data['date'] = record.subject
+            elif record.type == 'time':
+                elapsed['time'] = 'changed' if scene_data['time'] != record.subject else ''
+                scene_data['time'] = record.subject
+            else:
+                pass
+        elif record.type in ('scene-start', 'scene-end'):
+            if record.type == 'scene-end':
+                tmp.append(StoryCode('scene-elapsed', '', copy.deepcopy(elapsed)))
+            tmp.append(StoryCode(record.type, ""))
+        elif 'action' == record.type:
+            continue
+        elif 'text' == record.type:
+            continue
+        elif 'br' == record.type:
+            continue
+        elif 'indent' == record.type:
+            continue
+        else:
+            continue
+
+    return StoryCodeData(tmp)
